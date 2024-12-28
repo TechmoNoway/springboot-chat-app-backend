bb40f272cc6c7dc9e2cf5bd0cb062b0e0e05dff5
TechmoNoway <cuty0406@outlook.com>
2024-12-28 21:06:02 +0700

    Update docker file and change the config for the application

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,26 @@ EXPOSE 5672 15672 15674 61613
 ENV RABBITMQ_DEFAULT_USER=guest
 ENV RABBITMQ_DEFAULT_PASS=guest
 
-FROM eclipse-temurin:21-jdk-alpine as springapp
+FROM eclipse-temurin:21-jdk-alpine AS springapp
 COPY build/libs/*.jar /app/app.jar
 WORKDIR /app
 ENTRYPOINT ["java", "-jar", "app.jar"]
+
+FROM mysql:8.0 AS mysql
+ENV MYSQL_ROOT_PASSWORD=root_password
+ENV MYSQL_DATABASE=example_db
+ENV MYSQL_USER=user
+ENV MYSQL_PASSWORD=user_password
+EXPOSE 3306
+CMD ["mysqld"]
+
+FROM redis:latest AS redis
+EXPOSE 6379
+CMD ["redis-server"]
+
+
+
+
 
 
 
--- src/main/java/com/trickynguci/civicmessagerbackend/CivicMessagerBackendApplication.java
+++ src/main/java/com/trickynguci/civicmessagerbackend/CivicMessagerBackendApplication.java
@@ -1,12 +1,8 @@
 package com.trickynguci.civicmessagerbackend;
 
-import com.trickynguci.civicmessagerbackend.model.User;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 public class CivicMessagerBackendApplication {
